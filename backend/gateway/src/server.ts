af70d19e72fc0a6bb1840ad0c2ecfe69067d5a67
Sudhanshu Baberwal <24bcs147@iiitdwd.ac.in>
2026-08-18 21:26:27 +0530

    Allow origin

--- backend/gateway/src/server.ts
+++ backend/gateway/src/server.ts
@@ -3,7 +3,7 @@ import app from "./app.js";
 import env  from "./config/env.js";
 dotenv.config();
 
-app.listen(env.PORT, () => {
+app.listen(env.PORT,'0.0.0.0', () => {
   console.log("================================");
   console.log("Gateway Started 🚀");
   console.log(`http://localhost:${env.PORT}`);
