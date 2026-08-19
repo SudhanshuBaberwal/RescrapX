8d4bb0de3e362aae7060c629ea9bd26c8bfc76be
Sudhanshu Baberwal <24bcs147@iiitdwd.ac.in>
2026-08-19 14:30:29 +0530

    hi

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -3,7 +3,7 @@ import axios, { AxiosError, InternalAxiosRequestConfig } from "axios";
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
-  timeout: 10000000000000,
+  timeout: 60000,
   headers: {
     "Content-Type": "application/json",
   },
