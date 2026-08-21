28278b67a76134b86440f4d342ffdddf90a513ff
Sudhanshu Baberwal <24bcs147@iiitdwd.ac.in>
2026-08-22 00:16:29 +0530

    Fix

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, InternalAxiosRequestConfig } from "axios";
-// process.env.NEXT_PUBLIC_API_URL || 
+ 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
   timeout: 60000,
   headers: {
