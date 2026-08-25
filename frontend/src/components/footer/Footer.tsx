1c3d36bb8c574802a0d5dc2e95391b79ab7011d8
Sudhanshu Baberwal <24bcs147@iiitdwd.ac.in>
2026-08-25 12:59:08 +0530

    Fix Partner Page

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -32,7 +32,10 @@ export default function Footer() {
 
   const handlePartnerSignup = async () => {
     if (loading || isSuccess) return;
-
+    if (!userData){
+      router.push("/login")
+      return;
+    }
     setLoading(true);
     try {
       const result = await setPartner();
